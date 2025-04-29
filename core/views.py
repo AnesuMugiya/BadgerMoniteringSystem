e4f3a8633e4abb977563329bedb724dcfcfa4847
AnesuMugiya <71221500+AnesuMugiya@users.noreply.github.com>
2025-04-29 20:57:01 +0200

    Views.py for core app to render dashboard

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+# Log in required decorator ensures that only logged in users can access the dashboard
+@login_required
 def dashboard(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+    return render(request, 'core/dashboard.html')
